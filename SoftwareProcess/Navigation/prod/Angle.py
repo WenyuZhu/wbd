362908e96f69d910cdc2d6df59167546a8d9e840
artha <artha@172.17.66.206>
2016-10-07 18:15:16 -0500

    revised version after viewing angletest.py

--- SoftwareProcess/Navigation/prod/Angle.py
+++ SoftwareProcess/Navigation/prod/Angle.py
@@ -5,55 +5,63 @@ Created on Sep 1, 2016
 '''
 from math import degrees
 from _ast import Num
+import re
+
 class Angle():
     def __init__(self):
         # self.angle = ...       set to 0 degrees 0 minutes
-        self.angle_value = 0;
+        self.angle_value = 0.0;
         pass
     
-    def setDegrees(self, degrees):
+    def setDegrees(self, degrees=0.0):
         if not (isinstance(degrees, int) or isinstance(degrees, float)):
-            raise ValueError('Angle,setDgree:  Value is not integer or float, please retry')
+            raise ValueError('Angle.setDegrees:  Value is not integer or float, please retry')
+        degrees = (float)(degrees)
+#         degrees = round(degrees,1)
+        
         if degrees < 0 :
+            dec=degrees%(-1.0)
+            dec=dec*60
+            dec=round(dec,1)
+            dec=dec/60
+            degrees = int(degrees)
+            degrees = degrees + dec
             degrees = -degrees
             degrees = degrees % 360
             degrees = 360 - degrees
         else:
+            dec=degrees%1.0
+            dec=dec*60
+            dec=round(dec,1)
+            dec=dec/60
+            degrees=int(degrees)
+            degrees = degrees + dec
             degrees = degrees % 360
+        degrees=(float)(degrees)
         self.angle_value = degrees
         
         return self.angle_value
         pass
     
     def DegreeTransform(self, degrees):
-        for index in range(len(degrees)):
-            if degrees[index] == 'd':
-                if index == 0 :
-                    raise ValueError('Angle,setDegreesAndMinutes:  degree portion is needed')
-                if index == len(degrees) - 1:
-                    raise ValueError('Angle,setDegreesAndMinutes:  minute portion is needed')
-                temp = degrees[ :index]
-                try:
-                    a = int(temp)
-                except:
-                    raise ValueError('Angle,setDegreesAndMinutes:  degree portion has to be integer')
-                dec = degrees[index + 1:]
-                try:
-                    b = float(dec)
-                except:
-                    raise ValueError('Angle,setDegreesAndMinutes:  minute portion has to integer or float')
-                if b < 0:
-                    raise ValueError('Angle,setDegreesAndMinutes:  minute portion has to be a positive number')
-            if index == len(degrees) - 1 and not degrees[index] == 'd':
-                raise ValueError('Angle,setDegreesAndMinutes:  the separator d is not found')
-        carrier = int(b / 60)
-        b = b % 60
-        b = b / 60 + carrier
-        if a < 0:
-            a = a - b
+        pattern = re.compile(r'(^-?\d+d\d+\.\d$)|(^-?\d+d\d+$)')
+        ifmatch = pattern.match(degrees)
+        if not ifmatch:
+            raise ValueError('Angle.setDegreesAndMinutes: angleString violates the parameter specifications')
         else:
-            a = a + b
-        return a
+            for index in range(len(degrees)):
+                if degrees[index]=='d':
+                    break
+            deg=int(degrees[ :index])
+            dec=float(degrees[index+1: ])
+        carrier = int(dec / 60)
+        dec = dec % 60
+        dec = dec / 60 + carrier
+        if deg < 0:
+            deg = deg - dec
+        else:
+            deg = deg + dec
+        return deg
         pass
     
     def setDegreesAndMinutes(self, degrees):
@@ -69,9 +77,11 @@ class Angle():
         return self.angle_value
         pass
     
-    def add(self, angle):
+    def add(self, angle=None):
+        if angle==None:
+            raise ValueError('Angle.add: angle is needed')
         if not isinstance(angle, Angle):
-            raise ValueError('Angle, add:  angle is not a instance of Angle')
+            raise ValueError('Angle.add:  angle is not a instance of Angle')
         angleOut = angle.getDegrees()
         res = self.angle_value + angleOut
         if res < 0:
@@ -84,9 +94,11 @@ class Angle():
         return self.angle_value
         pass
     
-    def subtract(self, angle):
+    def subtract(self, angle=None):
+        if angle==None:
+            raise ValueError('Angle.subtract: angle is needed')
         if not isinstance(angle, Angle):
-            raise ValueError('Angle, subtract:  angle is not a instance of Angle')
+            raise ValueError('Angle.subtract:  angle is not a instance of Angle')
         angleOut = angle.getDegrees()
         res = self.angle_value - angleOut
         if res < 0:
@@ -99,13 +111,15 @@ class Angle():
         return self.angle_value
         pass
     
-    def compare(self, angle):
+    def compare(self, angle=None):
+        if angle == None:
+            raise ValueError('Angle.compare: angle is needed')
         if not isinstance(angle, Angle):
-            raise ValueError('Angle,compare:  angle is not a instance of Angle')
+            raise ValueError('Angle.compare:  angle is not a instance of Angle')
         angleOut = angle.getDegrees()
         if self.angle_value > angleOut:
             return 1
-        elif self.angle_value > angleOut:
+        elif self.angle_value == angleOut:
             return 0
         else:
             return -1
@@ -122,5 +136,6 @@ class Angle():
     
     def getDegrees(self):
         return self.angle_value
+#         return round(self.angle_value,1)
         pass
     
